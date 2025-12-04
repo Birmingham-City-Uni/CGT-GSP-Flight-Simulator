864e2c9d5a5b22000ec421f2c0abe519ff3c371b
Ben <benjamin.haskins@mail.bcu.ac.uk>
2025-12-04 11:00:32 +0000

    Player Cam
    The player can move the camera by pressing C and when released snaps back to facing Forward

--- FlightSim1/Assets/Interactions/Scripts/MouseLook.cs
+++ FlightSim1/Assets/Interactions/Scripts/MouseLook.cs
@@ -2,29 +2,47 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    //Mouse sensitivty
     public float MouseSense = 400f;
 
+    //Object the camera rotates around
     public Transform PlayerCam;
 
+    //Holds the players roataion
     float xRota = 0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //Locks the player curser
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * MouseSense * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * MouseSense * Time.deltaTime;
+        //When the C key is held down the player can move the camaera
+        if (Input.GetKey(KeyCode.C))
+        {
+            //Gets the inputs from the mouse and multiplys by preset sensitivty and applies it over time  
+            float mouseX = Input.GetAxis("Mouse X") * MouseSense * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * MouseSense * Time.deltaTime;
 
-        xRota -= mouseY;
-        //xRota = Mathf.Clamp(xRota, -90f, 90f);
+            //Prevents the player from looking too far up or down 
+            xRota -= mouseY;
+            xRota = Mathf.Clamp(xRota, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRota, 0f, 0f);
+            //Applies the transformation
+            transform.localRotation = Quaternion.Euler(xRota, 0f, 0f);
+            PlayerCam.Rotate(Vector3.up * mouseX);
+        }
 
-        PlayerCam.Rotate(Vector3.up * mouseX);
+        //When the C key is released it snaps the view back to fordward facing 
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            //Applies transformation
+            transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            PlayerCam.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        }
     }
 }
