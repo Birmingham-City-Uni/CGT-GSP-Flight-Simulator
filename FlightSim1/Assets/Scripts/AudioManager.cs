6ca1cf8528388bf08a1f9f76e870a4a4069936df
Jacob <jacob.beasonentwistle@mail.bcu.ac.uk>
2025-12-11 12:04:44 +0000

    Implemented audio script
    - Added audio clips (from the Audio team) into the project
    - Currently attached engine sound switching logic to the enemy's speed

--- FlightSim1/Assets/Scripts/AudioManager.cs
+++ FlightSim1/Assets/Scripts/AudioManager.cs
@@ -1,5 +1,8 @@
+using NUnit.Framework.Constraints;
 using System;
 using UnityEngine;
+
+public enum EngineState { LOW, MID, HIGH, HIGH_VTOL };
 
 public class AudioManager : MonoBehaviour
 {
@@ -10,35 +13,114 @@ public class AudioManager : MonoBehaviour
     [SerializeField] AudioSource SFXSource;
 
     [Header("-----------Clips Source-----------")]
-    public AudioClip backgroundMusic;   // [EXAMPLE]
-    public AudioClip crash;             // [EXAMPLE]
-    public AudioClip engineStage1;
-    public AudioClip engineStage2;
-    public AudioClip engineStage3;
+    //public AudioClip backgroundMusic;   // [EXAMPLE]
+    //public AudioClip crash;             // [EXAMPLE]
+    public AudioClip lowThrottle;
+    public AudioClip midThrottle;
+    public AudioClip highThrottle;
+    public AudioClip highThrottleVTOL;
+
+    [Header("-----------Player & Enemy references-----------")]
+    public EnemyAI enemy;
+    private EngineState currentState = EngineState.LOW;
 
     private void Start()
     {
-        // [EXAMPLE]
-        // Select the audio clip
-        musicSource.clip = backgroundMusic;
-        // Background music would go here as it plays from beginning
-        musicSource.Play();      // Plays the music
+        if (enemy == null)
+        {
+            enemy = GameObject.FindGameObjectWithTag("Enemy").GetComponent<EnemyAI>();
+        }
     }
 
     void Update()
     {
-        // [EXAMPLE]
-        if (true)
+        switch (currentState)
         {
-            // Select an audio clip for the audio source
-            SFXSource.clip = crash;
-            // Play the clip
-            SFXSource.PlayOneShot(crash, 0.8f);     // PlayOneShot(clip, volume) plays the sound without interrupting the other sounds going on
+            case EngineState.LOW:
+                LowEngine();
+                break;
+            case EngineState.MID:
+                MidEngine();
+                break;
+            case EngineState.HIGH:
+                HighEngine();
+                break;
+            case EngineState.HIGH_VTOL:
+                HighVTOLEngine();
+                break;
         }
 
-        // Play different engine sounds depending on speed
-        //if (player.velocity <= slow) { SFXSource.loop = true, SFXSource.Play(engineStage1) }
-        //if (player.velocity > slow && velocity < fast) { SFXSource.loop = true, SFXSource.Play(engineStage2) }
-        //if (player.velocity > fast) { SFXSource.loop = true, SFXSource.Play(engineStage3) }
+        //Debug.Log("Playing sound: " + SFXSource.clip);
+        //Debug.Log("[CURRENT ENGINE STATE: " + currentState + "]");
+    }
+
+    // Plays the low throttle sound
+    void LowEngine()
+    {
+        if (SFXSource.clip != lowThrottle)
+        {
+            SFXSource.clip = lowThrottle;
+            SFXSource.loop = true;
+            SFXSource.Play();
+        }
+
+        // The mid range speed
+        if (enemy.moveSpeed > 4)
+        {
+            currentState = EngineState.MID;
+        }
+    }
+    // Plays the mid throttle sound
+    void MidEngine()
+    {
+        if (SFXSource.clip != midThrottle)
+        {
+            SFXSource.clip = midThrottle;
+            SFXSource.loop = true;
+            SFXSource.Play();
+        }
+
+        // The high range speed
+        if (enemy.moveSpeed > 5)
+        {
+            currentState = EngineState.HIGH;
+        }
+        // The low range speed
+        if (enemy.moveSpeed < 5)
+        {
+            currentState = EngineState.LOW;
+        }
+    }
+    // Plays the high throttle sound (for regular planes)
+    void HighEngine()
+    {
+        if (SFXSource.clip != highThrottle)
+        {
+            SFXSource.clip = highThrottle;
+            SFXSource.loop = true;
+            SFXSource.Play();
+        }
+
+        // The mid range speed
+        if (enemy.moveSpeed < 6)
+        {
+            currentState = EngineState.MID;
+        }
+    }
+    // Plays the high throttle sound (for VTOL planes)
+    void HighVTOLEngine()
+    {
+        if (SFXSource.clip != highThrottleVTOL)
+        {
+            SFXSource.clip = highThrottleVTOL;
+            SFXSource.loop = true;
+            SFXSource.Play();
+        }
+
+        // The mid range speed
+        if (enemy.moveSpeed < 6)
+        {
+            currentState = EngineState.MID;
+        }
     }
 }
